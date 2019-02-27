96dbd9dd2e3b6690fe2436ec5d315190d88e6932
endotnick <endotnick@gmail.com>
2019-02-26 22:12:55 -0800

    add title to movie faker

--- test/fabricators/movie_fabricator.rb
+++ test/fabricators/movie_fabricator.rb
@@ -1,3 +1,6 @@
 Fabricator(:movie) do
+  # Faker does not have Faker::Movie.title
+  # faked title will not correspond to tmdb title
+  title { Faker::Book.unique.title }
   tmdb_id { Faker::Number.between(1, 150000) }
 end
