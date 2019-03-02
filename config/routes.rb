7ff8eb2986ca4c209e99f3978941486388be37c3
endotnick <endotnick@gmail.com>
2019-03-02 12:12:17 -0800

    add find_user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
+  post 'find_user' => 'users#find'
   get '/users/:id/reviews' => 'users#reviews'
   get '/users/:id/views' => 'users#views'
 
    # User Routes
-  get '/signup' => 'users#new'
   resources :users, only: %i[show update create]
   resources :reviews, only: %i[create edit update destroy]
   resources :views, only: %i[create destroy]
