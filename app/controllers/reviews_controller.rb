bf8e3ac2eda9881bca5c0b152e8c70baa63b26ce
endotnick <endotnick@gmail.com>
2019-03-02 12:12:43 -0800

    add auth before_actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :update, :destroy]
+  before_action :set_review, only: %i[show update destroy]
+  before_action :authenticate_user, only: %i[update destroy]
 
   # GET /reviews/1
   def show
