23f2d11b74c1a86ec18333e3bab0bf176917f942
endotnick <endotnick@gmail.com>
2019-03-10 14:29:23 -0700

    extracts user reviews to own endpoint

--- app/serializers/user_review_serializer.rb
+++ app/serializers/user_review_serializer.rb
@@ -1,7 +1,5 @@
 class UserReviewSerializer < ActiveModel::Serializer
   attributes %i[id content rating movie_title movie_id updated_at]
-  belongs_to :reviewer
-  belongs_to :movie
 
   def movie_title
     object.movie.title
